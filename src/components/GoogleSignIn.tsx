b98d5880a18cd4ab9f4a8496d6c65f13eedc57aa
Claudio Ceppi <101994228+ClaudioCeppi83@users.noreply.github.com>
2026-07-19 21:15:58 +0200

    fix: remove unnecessary Google API scopes
    Reduced the scope of the Google OAuth token request by removing generative-language and keep permissions to adhere to the principle of least privilege.

--- src/components/GoogleSignIn.tsx
+++ src/components/GoogleSignIn.tsx
@@ -29,7 +29,7 @@ export default function GoogleSignIn({ onLogin, clientId, authorizedEmail }: Goo
         try {
           const client = window.google.accounts.oauth2.initTokenClient({
             client_id: clientId,
-            scope: "https://www.googleapis.com/auth/spreadsheets https://www.googleapis.com/auth/drive.file https://www.googleapis.com/auth/drive.readonly https://www.googleapis.com/auth/generative-language https://www.googleapis.com/auth/tasks https://www.googleapis.com/auth/keep",
+            scope: "https://www.googleapis.com/auth/spreadsheets https://www.googleapis.com/auth/drive.file https://www.googleapis.com/auth/drive.readonly https://www.googleapis.com/auth/tasks",
             callback: async (response: any) => {
               setLoadingGoogle(false);
               if (response.error) {
